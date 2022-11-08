0d3cc1ba08004d092ccf6cb7979a9a88783e5696
karmjeetbanga <karmjeet.k@adorebeauty.com.au>
2022-11-08 16:20:09 +1100

    added css to cart

--- Screens/CartScreen.jsx
+++ Screens/CartScreen.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState, useContext } from "react";
 
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 import { CartContext } from "../CartContext";
-import { FontAwesome } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function CartScreen() {
   const { cartItems, getItemsCount, getTotalPrice, removeItemFromCart } =
@@ -15,9 +22,20 @@ export default function CartScreen() {
       setTotal(getTotalPrice());
     });
     return (
-      <View style={styles.cartLineTotal}>
-        <Text style={[styles.lineLeft, styles.lineTotal]}>Total</Text>
-        <Text style={styles.lineRight}>$ {total}</Text>
+      <View style={styles.cartView}>
+        <View style={styles.cartLineTotal}>
+          <Text style={styles.subtotal}>Subtotal </Text>
+          <Text style={styles.subtotal}>${total}</Text>
+        </View>
+
+        <View>
+          <Text style={styles.cartText}>
+            Shipping and taxes calculated at checkout.
+          </Text>
+          <TouchableOpacity style={styles.checkoutButton}>
+            <Text>checkout </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -25,21 +43,28 @@ export default function CartScreen() {
   function renderCartItem({ item }) {
     return (
       <View style={styles.cartLine}>
-        <Text style={styles.lineLeft}>
-          {item.product.name} x {item.qty}
-        </Text>
-        <FontAwesome
-          name="remove"
-          style={styles.remove}
-          size={20}
-          color="black"
-          onPress={() => {
-            removeItemFromCart(item.id);
-          }}
-        />
-        <Text style={styles.lineRight}>
-          {item.product.price_sign} {item.totalPrice}
-        </Text>
+        <View style={styles.imageView}>
+          <Image style={styles.image} source={{ uri: item.product.image }} />
+        </View>
+        <View style={styles.infoView}>
+          <View style={styles.lineTop}>
+            <Text style={styles.lineLeftTop}>{item.product.title}</Text>
+            <Text style={styles.lineRightTop}>${item.product.price}</Text>
+          </View>
+
+          <View style={styles.lineBottom}>
+            <Text style={styles.qty}>Qty: {item.qty}</Text>
+            <AntDesign
+              name="delete"
+              style={styles.remove}
+              size={20}
+              color="gray"
+              onPress={() => {
+                removeItemFromCart(item.id);
+              }}
+            />
+          </View>
+        </View>
       </View>
     );
   }
@@ -58,39 +83,110 @@ export default function CartScreen() {
 const styles = StyleSheet.create({
   cartLine: {
     flexDirection: "row",
+    backgroundColor: "white",
+    borderRadius: 16,
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 1,
+    marginVertical: 2.5,
+    padding: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      height: 0,
+      width: 0,
+    },
+  },
+
+  imageView: {
+    width: 100,
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 16,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 16,
+  },
+  infoView: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  lineTop: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  lineBottom: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  lineLeftTop: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  lineRightTop: {
+    fontSize: 16,
+    width: "30%",
+    textAlign: "right",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333333",
+  },
+  totalPrice: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333333",
+  },
+  qty: {
+    fontSize: 16,
+    color: "#333333",
+  },
+  remove: {
+    fontSize: 16,
+  },
+  cartView: {
+    marginVertical: 2.5,
+    padding: 10,
   },
   cartLineTotal: {
     flexDirection: "row",
-    borderTopColor: "#dddddd",
-    borderTopWidth: 1,
+    justifyContent: "space-between",
+    padding: 10,
   },
-  lineTotal: {
+  subtotal: {
+    fontSize: 16,
     fontWeight: "bold",
-  },
-  lineLeft: {
-    fontSize: 20,
-    lineHeight: 40,
     color: "#333333",
   },
-  lineRight: {
-    flex: 1,
-    fontSize: 20,
-    fontWeight: "bold",
-    lineHeight: 40,
-    color: "#333333",
-    textAlign: "right",
+  checkoutButton: {
+    backgroundColor: "#f0c14b",
+    borderRadius: 4,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
   },
+  cartText: {
+    fontSize: 12,
+    color: "#a9a9a10",
+    textAlign: "left",
+  },
+
   itemsList: {
     backgroundColor: "#eeeeee",
-    padding: 16,
   },
   itemsListContainer: {
     backgroundColor: "#eeeeee",
     paddingVertical: 8,
     marginHorizontal: 8,
-  },
-  remove: {
-    lineHeight: 40,
-    marginHorizontal: 20,
   },
 });
