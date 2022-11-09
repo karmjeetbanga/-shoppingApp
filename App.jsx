b011a9e136429352f5f210ee0553f3a3e415351d
karmjeetbanga <karmjeet.k@adorebeauty.com.au>
2022-11-09 14:24:04 +1100

    cleanup app.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ProductScreen from "./Screens/ProductScreen";
@@ -39,12 +39,3 @@ export default function App() {
     </CartProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
